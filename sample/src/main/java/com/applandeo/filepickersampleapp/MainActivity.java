943ec4f7ca272645885ceaa527f3130f9ad7d0d9
Mateusz Kornakiewicz <mateusz.kornakiewicz@applandeo.com>
2017-08-30 14:51:41 +0200

    Added icons for all screen resolution

--- sample/src/main/java/com/applandeo/filepickersampleapp/MainActivity.java
+++ sample/src/main/java/com/applandeo/filepickersampleapp/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         openPicker.setOnClickListener(
                 view -> new FilePicker.Builder(this, listener)
-//                        .setMainDirectory(Environment.getExternalStorageDirectory().getPath())
+                        .setMainDirectory("/")
                         .hideFiles(false) //to show only directories
 //                        .directory("/")
                         .show());
